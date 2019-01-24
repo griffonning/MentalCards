6a00d8269224894ceb4b252968e0d04ef1b35123
hoshi <hoshi@hoshis-MacBook-Pro.local>
2019-01-24 01:24:23 +0100

    Initial Commit

---
+++ MentalCards/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MentalCards
+//
+//  Created by hoshi on 24.01.19.
+//  Copyright © 2019 hoshi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
